7f9bcc202d1e67bb9a390c4ad5d40b703a501fe3
jeanpaulvandeursen <github@vandeursen.org>
2015-03-29 12:14:08 +0200

    initial

---
+++ GroovyTest/src/org/vandeursen/Concepts.groovy
@@ -0,0 +1,14 @@
+package org.vandeursen
+
+class Concepts {
+	
+	static void main(def args){
+		def firstName='Jean-Paul'
+		def lastName='van Deursen'
+		def name="$firstName $lastName"
+		println name
+		def myList=[1,2,3,4]
+		myList.each { println it }
+		assert(name=="Jean-Paul van Deursen")
+	}
+}
