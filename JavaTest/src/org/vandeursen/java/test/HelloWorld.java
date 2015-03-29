028b233fd18a39694e57b988e688f3362c420975
jeanpaulvandeursen <github@vandeursen.org>
2015-03-29 22:56:35 +0200

    testing a tag

--- JavaTest/src/org/vandeursen/java/test/HelloWorld.java
+++ JavaTest/src/org/vandeursen/java/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Git world!");
 		System.out.println("Here I am!");
+		System.out.println("And this is a new tag");
 	}
 
 }
