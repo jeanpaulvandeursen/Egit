cc90d3485664d764814c8df611c909870876afa3
jeanpaulvandeursen <github@vandeursen.org>
2015-11-26 20:24:54 +0100

    added cats and dogs

--- JavaTest/src/org/vandeursen/java/test/CastInstanceOfTester.java
+++ JavaTest/src/org/vandeursen/java/test/CastInstanceOfTester.java
@@ -1,11 +1,27 @@
 package org.vandeursen.java.test;
 
+class Animal{}
+
+class Mammal extends Animal{}
+
+class Cat extends Mammal{}
+
+class Dog extends Mammal{}
+
 public class CastInstanceOfTester {
 
 	public static void main(String[] args) {
 		Object Monkey = (Object) returnString();
 		if (Monkey instanceof String) {
 			System.out.println("true" + Monkey);
+		} else {
+			System.out.println("false");
+		}
+		Cat c = new Cat();
+		Mammal cat = (Mammal)c;
+		if (cat instanceof Cat) {
+			// the upcast to Mammal does not mean the original type is lost
+			System.out.println("true");
 		} else {
 			System.out.println("false");
 		}
